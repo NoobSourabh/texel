e0bb246dc55acbf2671a65e11fdfddb8473da0d0
Sourabh Chouhan <you@example.com>
2025-01-14 19:23:04 +0530

    Home card responsiveness fixed

--- src/Components/Sections/Schools/index.tsx
+++ src/Components/Sections/Schools/index.tsx
@@ -14,8 +14,6 @@ const School = () => {
                     Texel is started with a very <strong className="highlighted-text">clear purpose</strong> in mind that is to provide actually <strong className="highlighted-text">helpful</strong> and <strong className="highlighted-text">meaningful</strong> support to schools in their journeys in such a way that it prioritizes new ideas and <strong className="highlighted-text">innovative concepts</strong>.
                 </p>
             </div>
-
-
             <Para />
             <div className="w-[85vw] md:h-28 md:mb-0 mb-[130px] opacity-90 text-center mx-auto">
                 <span className="text-[#727272] md:text-[32px]  text-[15px]  font-normal font-msUIGothic md:leading-[29px] leading-snug  tracking-tight">Join us in our journey to help </span>
@@ -31,24 +29,3 @@ const School = () => {
 export default School
 
 
-
-{/* <div className="sec-1-para">
-                <p className="sec-1-p">
-                    Inside every school lies a very precious resource – the <strong>eager students</strong> with a <strong>passion</strong> for learning, <strong>dedicated teachers</strong> who share knowledge and promote growth, and <strong>caring parents</strong> who are deeply invested in their children’s success. However, this great potential within everyone is suppressed due to many reasons such as <strong>complicated</strong>, <strong>outdated</strong>, and <strong>messy systems</strong>. Fortunately, Texel has now entered to change the game and transform educational technology for the better.
-                </p>
-                <p className="sec-1-p">
-                    Texel is started with a very <strong>clear purpose</strong> in mind that is to provide actually <strong>helpful</strong> and <strong>meaningful</strong> support to schools in their journeys in such a way that it prioritizes new ideas and <strong>innovative concepts</strong>.
-                </p>
-            </div> */}
-
-{/* <div className="mission-statement">
-                <span className="text-muted">Inside every school lies a very precious resource - </span>
-                <span className="highlighted-text">the eager students with a passion for learning</span>
-                <span className="text-muted">, </span>
-                <span className="highlighted-text">dedicated teachers</span>
-                <span className="text-muted"> who share knowledge and promote growth and </span>
-                <span className="highlighted-text">caring parents</span>
-                <span className="text-muted"> who are deeply invested in their children’s success. However this great potential within everyone is suppressed due to many reasons such as - </span>
-                <span className="highlighted-text">complicated, outdated and messy systems</span>
-                <span className="text-muted">. Fortunately, Texel has now entered to change the game and transform educational technology for the better.</span>
-            </div> */}
--- src/Components/UI/Cards/HomeCard/index.tsx
+++ src/Components/UI/Cards/HomeCard/index.tsx
@@ -1,31 +1,22 @@
-import './styles.css'
+import './styles.css';
 
 type propType = {
-  url: string,
-  title: string,
-  description: string
-}
+  url: string;
+  title: string;
+  description: string;
+};
+
 const HomeCard = ({ url, title, description }: propType) => {
   return (
-    // <div className="homecard-container" >
-    //   <div className='home-img-container'>
-    //     <img className="" src={url} />
-    //   </div>
-    //   <h3 className="home-card-title">{title}</h3>
-    //   <div className='red-border'> </div>
-    //   <p className="home-card-description">{description}</p>
-    // </div >
-    // <div className="h-[296px] justify-start items-center gap-[30px] inline-flex">
-
-    <div className="mob-home-card md:home-card">
-      <div className="md:home-card-header mob-home-card-header ">
-        <img className="mob-home-card-image  md:home-card-image" src={url} />
+    <div className="home-card mob-home-card">
+      <div className="home-card-header mob-home-card-header">
+        <img className="home-card-image mob-home-card-image" src={url} alt={title} />
       </div>
-      <h3 className="mob-home-card-title md:home-card-title">{title}</h3>
-        <div className="home-card-divider"></div>
-      <p className="mob-home-card-description md:home-card-description">{description}</p>
-      </div>
-  )
-}
+      <h3 className="home-card-title mob-home-card-title">{title}</h3>
+      <div className="home-card-divider mob-home-card-divider"></div>
+      <p className="home-card-description mob-home-card-description">{description}</p>
+    </div>
+  );
+};
 
-export default HomeCard
+export default HomeCard;
