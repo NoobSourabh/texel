d5702e55701c2fce0d0c5db551b9542130c94325
Sourabh Chouhan <you@example.com>
2025-01-15 11:49:17 +0530

    Feature(home)  svgs fixed

--- src/Components/Sections/Home/Features/index.tsx
+++ src/Components/Sections/Home/Features/index.tsx
@@ -7,27 +7,27 @@ const Features = () => {
 
             <div className="w-[518px] h-[103px] text-center text-[#8a6d59] text-5xl font-normal font-konkhmer leading-[48px]">FEATURES <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /></div>
             <div className="ftr-cards">
-                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736230741/dee039ead5e4df247307897d969daddf_u3gr17.png"
+                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736921729/image-66_jzwq2x.svg"
                     heading="Attendance Tracking"
                     description="Simplify the process of recording attendance with intuitive dashboards, ensuring accurate and easily accessible records for all stakeholders."
                 />
-                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736230741/c1eca134d1a3c9536d3dd21e010c340b_drv0jp.png"
+                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736921565/image-65_wkkqhz.svg"
                     heading="Timetable Management"
                     description="Keep everyone informed with real-time updates on class schedules, making it easy for students, teachers, and parents to stay organized."
                 />
-                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736230740/8e1135f8669cfaac3976fc71ea42b3ca_xb8zcp.png"
+                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736921808/image-67_tortih.svg"
                     heading="Progress Reports"
                     description="Provide detailed insights into academic performance, combining attendance data and grades for comprehensive evaluations."
                 />
-                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736230794/homework-svgrepo-com_1_1_jnuvfw.png"
+                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736921803/image-68_ayxgre.svg"
                     heading="Homework Submission"
                     description="Simplify assignment management with easy submissions, allowing teachers to track progress and parents to monitor workload."
                 />
-                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736230741/f773fa6511fa5d3df879659f0ed821f9_vgs5ro.png"
+                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736921800/image-69_ojxsxi.svg"
                     heading="Notices & Announcements"
                     description="Facilitate effective communication through a centralized platform for important updates, ensuring everyone stays informed."
                 />
-                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736230741/379cc07f5de19b88052111fd322032d4_votn9r.png"
+                <FeatureCard iconUrl="https://res.cloudinary.com/dswwp9dju/image/upload/v1736921796/image-70_wmql6c.svg"
                     heading="GPS Tracking"
                     description="Enhance student safety with ID card tracking, securing access to attendance, transport updates, and real-time activity monitoring."
                 />
