7496a004698cb536e3af21ad6b6efe11b33379f5
Sourabh Chouhan <you@example.com>
2025-01-23 06:03:49 +0530

    paragraph mission

--- src/Components/Sections/Mission/paragraph.tsx
+++ src/Components/Sections/Mission/paragraph.tsx
@@ -3,20 +3,20 @@ const Paragraph = () => {
   return (
 
 
-    <div className="education-text-container">
-      <p className="education-text description">
+    <div className="w-full text-left md:px-0 px-[27px] md:w-[44.5vw]  opacity-90 md:mx-0 md:py-0 py-[-26px] ">
+      <p className="md:text-xl text-[13px] font-normal text-[#727272] font-ptsans leading-[29px] tracking-tight">
         Imagine a world where parents are doing exactly what teachers do—actively participating in their child's education. This application helps bridge that gap.
 
         <b className="text-black/60"> Education is a shared responsibility between parents and institutions. </b>
         Teachers involve children, but parents are usually kept in the dark and are generally uninformed about their child's school schedule.</p>
       <br />
-      <p className="education-text description">
+      <p className="md:text-xl text-[13px] font-normal text-[#727272] font-ptsans leading-[29px] tracking-tight">
         &nbsp;
         <b className="text-black/60">Children spend nearly half their waking hours in school, while parents have very little experience.</b>
         This application gives parents empowering information. The homework section explains assignments clearly, covered topics, and needed support areas. This is about making parents active participants in the education of their child. Schools also greatly benefit from this. Our application monitors teacher and student engagement.
       </p>
       <br />
-      <p className="education-text description">
+      <p className="md:text-xl text-[13px] font-normal text-[#727272] font-ptsans leading-[29px] tracking-tight">
 
         It helps schools in understanding methods of teaching, pinpointing areas where students would need special support.
         <b className="text-black/60"> “This is a very powerful tool that recognizes dedication and helps kids thrive” </b >.
@@ -26,7 +26,7 @@ const Paragraph = () => {
       </p>
       <br />
 
-      <p className="education-text description">
+      <p className="md:text-xl text-[13px] font-normal text-[#727272] font-ptsans leading-[29px] tracking-tight">
 
         We are empowering parents, teachers, and school administrators.The app transforms parents into active participants in their child's journey,
         <b className="text-black/60"> celebrating successes</b> &nbsp;
