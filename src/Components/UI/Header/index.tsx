074eef8fb909f3d4642b4d06fd81e8166fd23edf
Sourabh Chouhan <you@example.com>
2025-01-24 22:43:49 +0530

    code uncommented

--- src/Components/UI/Header/index.tsx
+++ src/Components/UI/Header/index.tsx
@@ -1,48 +1,48 @@
-// const Header = ({ onToggleNav }: { onToggleNav: () => void }) => {
-//     return (
-//         <nav className="flex justify-between items-center md:pl-10 bg-white top-0 w-full h-auto pt-[32px] md:pt-0 md:pb-0 md:pr-4 pr-0">
-//             <a href="/" className="flex items-center">
-//                 <img
-//                     className="mb-[-6px] overflow-hidden"
-//                     src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736533482/image-42_wrox1g.svg"
-//                     alt="Texel Logo"
-//                 />
-//                 <img
-//                     className="pt-[6px]"
-//                     src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736533529/image-43_whicv9.svg"
-//                     alt="Texel Text"
-//                 />
-//             </a>
+const Header = ({ onToggleNav }: { onToggleNav: () => void }) => {
+    return (
+        <nav className="flex justify-between items-center md:pl-10 bg-white top-0 w-full h-auto pt-[32px] md:pt-0 md:pb-0 md:pr-4 pr-0">
+            <a href="/" className="flex items-center">
+                <img
+                    className="mb-[-6px] overflow-hidden"
+                    src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736533482/image-42_wrox1g.svg"
+                    alt="Texel Logo"
+                />
+                <img
+                    className="pt-[6px]"
+                    src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736533529/image-43_whicv9.svg"
+                    alt="Texel Text"
+                />
+            </a>
 
-//             <div className="hidden md:flex items-end justify-between w-[423px] mr-[22px] mt-[8px] tracking-wider ">
-//                 <a href="/" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
-//                     Home
-//                 </a>
-//                 <a href="/for-schools" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
-//                     For Schools
-//                 </a>
-//                 <a href="/mission" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
-//                     Mission
-//                 </a>
-//                 <a href="/about" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
-//                     About Us
-//                 </a>
-//                 <a href="/contact" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
-//                     Contact Us
-//                 </a>
-//             </div>
+            <div className="hidden md:flex items-end justify-between w-[423px] mr-[22px] mt-[8px] tracking-wider ">
+                <a href="/" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
+                    Home
+                </a>
+                <a href="/for-schools" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
+                    For Schools
+                </a>
+                <a href="/mission" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
+                    Mission
+                </a>
+                <a href="/about" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
+                    About Us
+                </a>
+                <a href="/contact" className="text-[color:var(--second-text-color,#737373)] text-center font-montserrat text-[14px] font-bold leading-[24px]">
+                    Contact Us
+                </a>
+            </div>
 
-//             <button
-//                 onClick={onToggleNav}
-//                 className="block md:hidden pr-6 focus:outline-none"
-//                 aria-label="Toggle Navigation"
-//             >
-//                 <img
-//                     src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736522166/image-38_qerqqb.svg"
-//                     alt="Menu Icon"
-//                 />
-//             </button>
-//         </nav>
-//     );
-// };
-// export default Header;
+            <button
+                onClick={onToggleNav}
+                className="block md:hidden pr-6 focus:outline-none"
+                aria-label="Toggle Navigation"
+            >
+                <img
+                    src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736522166/image-38_qerqqb.svg"
+                    alt="Menu Icon"
+                />
+            </button>
+        </nav>
+    );
+};
+export default Header;
