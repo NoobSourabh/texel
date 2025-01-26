b965977727f3b373380ac9c0a439d54af5aa380c
Sourabh Chouhan <you@example.com>
2025-01-26 11:28:39 +0530

    vectors added

--- src/Components/Sections/Home/LastSection/index.tsx
+++ src/Components/Sections/Home/LastSection/index.tsx
@@ -1,15 +1,15 @@
 const LastSection = () => {
     return (
-        <div className="md:my-[46px] my-0 mt-[44px] md:pb-0  h-fit [@media(max-height:750px)]:min-h-[1000px]">
-            <div className="w-[100vw] text-center text-[#252b42] md:text-[32px] text-[26px] font-bold font-arimo italic md:pb-0 ">
+        <div className="md:my-[46px] mt-[24px] md:pb-[10vh] pb-8 h-fit [@media(max-height:750px)]:min-h-[1000px]">
+            <div className="w-[100vw] text-center text-[#252b42] md:text-[32px] text-[26px] font-bold font-arimo italic md:pb-0">
                 “ Quality comes without your permission “
             </div>
             <div
-                className="md:bg-[url('https://res.cloudinary.com/dswwp9dju/image/upload/v1736546539/image-46_ii49xl.svg')] bg-no-repeat lg:bg-center md:bg-[50%_20%] md:w-[100vw] md:h-[125vh] flex flex-col"
+                className="md:bg-[url('/assets-public/images/svgs/home-lastsection-vector.svg')] bg-no-repeat lg:bg-center md:bg-[50%_20%] md:w-[100vw] md:h-[125vh] flex flex-col justify-between "
                 style={{ backgroundSize: '100vw' }}
             >
-                <div className="flex flex-col items-center py-8">
-                    <div className="flex flex-col gap-6 text-center items-center w-full md:w-[602px] left-[61px]">
+                <div className="flex flex-col justify-between items-center py-8 mb-10">
+                    <div className="flex flex-col gap-6 text-center items-center w-full md:w-[602px] left-[61px] ">
                         <p className="text-gray text-[15px] md:text-2xl px-4 leading-[29px] tracking-tight">
                             At Texel, we believe that education thrives when it's a shared responsibility. Our
                             innovative application dismantles the barriers that keep parents in the dark, providing them
@@ -18,20 +18,22 @@ const LastSection = () => {
                         </p>
                         <img
                             className="md:hidden"
-                            src="https://res.cloudinary.com/dswwp9dju/image/upload/v1736849891/image-63_znh4xr.svg"
+                            src='/assets-public/images/svgs/home-lastsection-vector-mob.svg'
                             alt="Texel Illustration"
                         />
                     </div>
                 </div>
-                <div className="flex w-full flex-col lg:pt-[31vh] md:pt-[12vh] md:pl-[59px] md:pr-0 pl-[20px] pr-[20px] md:gap-2 gap-0 items-center md:items-start md:pb-0">
-                    <p className="md:w-[40vw] font-ptsans text-[11px] md:text-[22px] py-2 md:text-left text-center text-gray font-bold md:leading-[29px] leading-relaxed tracking-wide">
+                {/* <div className="flex w-full flex-col lg:pt-[31vh] md:pt-[12vh] md:pl-[59px] md:pr-0 pl-[20px] pr-[20px] md:gap-2 gap-0 items-center md:items-start md:pb-0"> */}
+                <div className="flex w-full flex-col md:pl-[59px] md:pr-0 pl-[20px] pr-[20px] ">
+                    <p className="font-ptsans md:w-[40vw]  text-[11px] md:text-[22px] py-2 md:text-left text-center text-gray font-bold md:leading-[29px] leading-tight tracking-tight">
+
                         By harnessing the power of technology, we empower schools with essential tools that monitor
                         engagement and highlight student achievements. Imagine a community where teachers can focus on
                         inspiring young minds, while parents celebrate milestones and tackle challenges together. With
                         Texel, we’re not just managing education; we’re enriching it, creating a vibrant ecosystem where
                         every user—teachers, students, and parents—feels inspired and informed.
                     </p>
-                    <div className="w-full md:text-left text-center lg:pt-0 md:pt-4 pt-0  md:w-full lg:w-[65vw] md:text-[#252b42] md:text-[32px] text-[19px] font-bold font-arimo italic md:leading-[43.52px] leading-relaxed">
+                    <div className="w-full md:text-left text-center md:w-full lg:w-[65vw] md:text-[#252b42] md:text-[32px] text-[19px] font-bold font-arimo italic md:leading-[43.52px] leading-relaxed">
                         “Join us in our mission to cultivate a future where education is a collaborative adventure, and
                         together, let’s illuminate the path to academic excellence, one child at a time.“
                     </div>
@@ -42,7 +44,6 @@ const LastSection = () => {
 };
 
 export default LastSection;
-
 
 
 // import './styles.css'
